6bbecc40de82a095e74e03d977f64f8c2210a726
Abdellah-Nsila <dawndawn074@gmail.com>
2025-02-16 16:16:12 +0100

    Work with variable condition

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 11:45:07 by abnsila           #+#    #+#             */
-/*   Updated: 2025/02/15 15:49:00 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/02/16 14:59:41 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 # define PHILOSOPHERS_H
 
 # include "functions.h"
+# include <math.h>
 # include <time.h>
 # include <sys/wait.h>
 # include <pthread.h>
 
-# define THREADS 10
+# define THREADS 2
 
 typedef struct s_philo
 {
 	pthread_mutex_t	mutex;
-	int				*i;
+	pthread_cond_t	cond;
+	int				fuel;
+	int				*arr;
 	int				score;
 }				t_philo;
 
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,64 +6,129 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:48:15 by abnsila           #+#    #+#             */
-/*   Updated: 2025/02/15 16:04:44 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/02/16 15:09:33 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-pthread_mutex_t	mutex;
-int	arr[THREADS] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+void	*ft_fuel_filling(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) arg;
+	pthread_mutex_lock(&philo->mutex);
+	philo->fuel += 10;
+	printf("Fuel: %d\n", philo->fuel);
+	pthread_mutex_unlock(&philo->mutex);
+	pthread_cond_signal(&philo->cond);
+	sleep(1);
+	return (NULL);
+}
+
+void	*ft_car(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) arg;
+	pthread_mutex_lock(&philo->mutex);
+	while (philo->fuel < 40)
+	{
+		printf("No Fuel, Waiting...\n");
+		pthread_cond_wait(&philo->cond, &philo->mutex);
+	}
+	philo->fuel -= 40;
+	printf("Got Fuel: %d\n", philo->fuel);
+	pthread_mutex_unlock(&philo->mutex);
+	return (NULL);
+}
 
 void	ft_init_philo(t_philo *philo)
 {
 	srand(time(NULL));
-	pthread_mutex_init(&philo->mutex, NULL);
 	ft_bzero(philo, sizeof(t_philo));
 	pthread_mutex_init(&philo->mutex, NULL);
-}
-
-void	*ft_get_unique_num(void *arg)
-{
-	int	i;
-
-	pthread_mutex_lock(&mutex);
-	i = *((int *)arg);
-	printf("%d\n", arr[i]);
-	free(arg);
-	pthread_mutex_unlock(&mutex);
-	return ((void *)(&arr[i]));
+	pthread_cond_init(&philo->cond, NULL);
 }
 
 int	main()
 {
 	t_philo		philo;
 	pthread_t	threads[THREADS];
-	int			i = 0;
-	int			*temp = 0;
-	int			sum = 0;
 
 	ft_init_philo(&philo);
-	while (i < THREADS)
-	{	
-		int *a = malloc(sizeof(int));
-		*a = i;
-		if (pthread_create(&threads[i], NULL, &ft_get_unique_num, (void *) a) != 0)
-			return (EXIT_FAILURE);
-		i++;
-	}
-	i = 0;
-	while (i < THREADS)
-	{
-		if (pthread_join(threads[i], (void **)&temp) != 0)
-			return (EXIT_FAILURE);
-		sum += *temp;
-		i++;
-	}
-	printf("\nSum of Num: %d\n", sum);
-	pthread_mutex_destroy(&mutex);
+	
+	if (pthread_create(&threads[0], NULL, &ft_fuel_filling, NULL) != 0)
+		return (EXIT_FAILURE);
+	if (pthread_create(&threads[1], NULL, &ft_car, NULL) != 0)
+		return (EXIT_FAILURE);
+		
+	if (pthread_join(threads[0], NULL) != 0)
+		return (EXIT_FAILURE);
+	if (pthread_join(threads[1], NULL) != 0)
+		return (EXIT_FAILURE);
+	
+	pthread_mutex_destroy(&philo.mutex);
+	pthread_cond_destroy(&philo.cond);
 	return (0);
 }
+
+
+//! Array sum using threads
+// pthread_mutex_t	mutex;
+// int	arr[THREADS] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+// void	ft_init_philo(t_philo *philo)
+// {
+// 	srand(time(NULL));
+// 	pthread_mutex_init(&philo->mutex, NULL);
+// 	ft_bzero(philo, sizeof(t_philo));
+// 	pthread_mutex_init(&philo->mutex, NULL);
+// }
+
+//? arr[i] = (rand() % (max - min)) + min;
+
+// void	*ft_get_unique_num(void *arg)
+// {
+// 	int	i;
+
+// 	pthread_mutex_lock(&mutex);
+// 	i = *((int *)arg);
+// 	printf("%d\n", arr[i]);
+// 	free(arg);
+// 	pthread_mutex_unlock(&mutex);
+// 	return ((void *)(&arr[i]));
+// }
+
+// int	main()
+// {
+// 	t_philo		philo;
+// 	pthread_t	threads[THREADS];
+// 	int			i = 0;
+// 	int			*temp = 0;
+// 	int			sum = 0;
+
+// 	ft_init_philo(&philo);
+// 	while (i < THREADS)
+// 	{	
+// 		int *a = malloc(sizeof(int));
+// 		*a = i;
+// 		if (pthread_create(&threads[i], NULL, &ft_get_unique_num, (void *) a) != 0)
+// 			return (EXIT_FAILURE);
+// 		i++;
+// 	}
+// 	i = 0;
+// 	while (i < THREADS)
+// 	{
+// 		if (pthread_join(threads[i], (void **)&temp) != 0)
+// 			return (EXIT_FAILURE);
+// 		sum += *temp;
+// 		i++;
+// 	}
+// 	printf("\nSum of Num: %d\n", sum);
+// 	pthread_mutex_destroy(&mutex);
+// 	return (0);
+// }
 
 //! You 10 threades, an array of numbers, each tread, print a unique prime number
 // pthread_mutex_t	mutex;
