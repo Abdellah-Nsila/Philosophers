6bcfbb640d255867672e28658b3305c74feaf5af
Abdellah-Nsila <dawndawn074@gmail.com>
2025-02-19 18:28:13 +0100

    Some Update

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,11 +6,11 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 19:11:35 by abnsila           #+#    #+#             */
-/*   Updated: 2025/02/18 19:36:21 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/02/19 18:28:06 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
 //TODO If you want complete the playlist and make the init struct for philo, and add peototype functiins and the flow in tldraw,
-// and seach in internet
+//TODO and seach in internet
