2c7cebc4dcac7448398514118b5d2c9ec7368844
Abdellah-Nsila <dawndawn074@gmail.com>
2025-04-07 19:02:56 +0100

    Try new version of usleep

--- philo_bonus/src/philo_time.c
+++ philo_bonus/src/philo_time.c
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 07:31:56 by abnsila           #+#    #+#             */
-/*   Updated: 2025/04/04 14:51:27 by abnsila          ###   ########.fr       */
+/*   Updated: 2025/04/07 17:09:46 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,46 @@ time_t	get_current_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
+// void	ft_usleep(t_philo *philo, time_t milliseconds)
+// {
+// 	(void)philo;
+// 	time_t	start;
+
+// 	start = get_current_time();
+// 	while (((get_current_time() - start) < milliseconds))
+// 	{
+// 		usleep(10000);
+// 		sem_wait(philo->done_sem.ptr);
+// 		if (philo->is_done)
+// 		{
+// 			sem_post(philo->done_sem.ptr);
+// 			return ;
+// 		}
+// 		sem_post(philo->done_sem.ptr);
+// 		usleep(10000);
+// 	}
+// }
+
 void	ft_usleep(t_philo *philo, time_t milliseconds)
 {
-	(void)philo;
-	time_t	start;
+	time_t start = get_current_time();
+	time_t elapsed;
 
-	start = get_current_time();
-	while ((get_current_time() - start) < milliseconds)
-	{
+	while ((elapsed = get_current_time() - start) < milliseconds) {
+		if (elapsed > milliseconds / 2) {
+			usleep(1000);  // Shorter sleeps as deadline approaches
+		} else {
+			usleep(500);
+		}
 		sem_wait(philo->done_sem.ptr);
-		if (philo->is_done)
-		{
+		if (philo->is_done) {
 			sem_post(philo->done_sem.ptr);
-			break ;
+			return;
 		}
 		sem_post(philo->done_sem.ptr);
-		usleep(100);
 	}
 }
+
 
 void	ft_start_delay(time_t start_time)
 {
